4aa30d25ccd39777238dcd25670f12689b958169
jorgegn15 <jorge323.n@gmail.com>
2024-06-05 08:01:16 -0500

    1 Commit
    1 Commit

---
+++ API.NET8/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using API.NET8.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.NET8.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ API.NET8/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace API.NET8.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int Edad { get; set; }
+    }
+}
